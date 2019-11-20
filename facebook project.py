af4c3146c5b708b2c0941195ba1c639f857b206e
fadim21-meet <57598436+fadim21-meet@users.noreply.github.com>
2019-11-20 20:05:05 +0200

    Add files via upload

--- facebook project.py
+++ facebook project.py
@@ -1,26 +1,49 @@
 class User():
-	def __init__(self,name,email,password):
+	def __init__(self,name,email,password,friends_list,posts):
+
+		friends_list = []
+		posts = []
 		self.name = name
 		self.email = email
 		self.password = password
 		self.friends_list=[]
 		self.posts =[]
+
 	def add_friend(self,email):
+		self.friends_list.append(email)
 		print(self.name + " has added " + email )
+
 	def remove_friend(self,email):
-		self.friends_list.pop(email)
+		self.friends_list.remove(email)
 		print(self.name + " has removed " + email + " from friends_list " )
+
 	def post(self,text):
 		print(self.name + " has posted : " + text )
+
 	def get_userInfo(self):
-		print("name" + self.name )
-		print("email" + self.email )
-		print("password" + self.password )
-		print("friends" + self.friends_list )
-		print("posts" + self.post )
-user1 = User("Fadi","fadithebeast@gmail.com","fadithebeast@@##")
-user2 = User("trevor","trevorlinkolen@gmail.com","trevorlinkolen100%")
+		print(" name " + self.name )
+		print(" email " + self.email )
+		print(" password " + self.password )
+		print(" friends " + str(self.friends_list) )
+		print("posts" + str(self.post) )
+
+user1 = User("Fadi","fadithebeast@gmail.com","fadithebeast@@##"," som3a "," how are you ")
+user2 = User("trevor","trevorlinkolen@gmail.com","trevorlinkolen100%"," aljabra "," no pain no gain ")
+
 user1.add_friend("thebeast@gmail.com") 
 
+user1.post(" hello friends ")
+
+user1.get_userInfo()
+user2.get_userInfo()
+
+user1.remove_friend("thebeast@gmail.com")
 
 
+class post(object):
+	def __init__(self,num_likes,num_comments):
+
+		self.num_likes = 0
+		self.num_comments = 0
+		
+	
