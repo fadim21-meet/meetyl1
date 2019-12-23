de4dfde65faf2c4799c24e70592bbe166143ecff
fadim21-meet <57598436+fadim21-meet@users.noreply.github.com>
2019-12-23 19:34:13 +0200

    Add files via upload

--- lab 2.py
+++ lab 2.py
@@ -7,12 +7,14 @@ def creat_fun(l1,l2):
 		if num in l2:
 			x.append(num)
 	return (x)
-#x = creat_fun(list1,list2)
-#print(x)
+x = creat_fun(list1,list2)
+print(x)
 
 
 
 encoder_caesar =
 {'a':'d','b':'e','c':'f','d':'g','e':'h','f':'i','g':'j','h':'k','i':'l','j':'m','k':'n','l':'o','m':'p','n':'q','o':'r','p':'s','q':'t','r':'u','s':'v','t':'w','u':'x','v':'y','w':'z','x':'a','y':'b','z':'c',}
-
+def mohammad(stringin):
+	stringin=input("can you type anything?")
+	print(encoder_caesar[stringin])
 
